38c8bb5b3ed3c534491766fd8249d1d36b9cb170
rupesh-dev30 <cgcrupesh@gmail.com>
2025-04-12 04:20:07 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./global.css"
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-red-800 font-bold items-center underline">HLO EVERYONE.</Text>
+    </View>
+  );
+}
